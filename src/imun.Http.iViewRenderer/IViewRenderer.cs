7963e87b14cba7263660b7157dab2a7942072c46
iman nemati <nemati@gmail.com>
2019-12-11 10:34:21 +0330

    Add check for exceptions

--- src/imun.Http.iViewRenderer/IViewRenderer.cs
+++ src/imun.Http.iViewRenderer/IViewRenderer.cs
@@ -4,7 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace imun.Http.iViewRenderer {
     public interface IViewRenderer {
-        Task<string> RenderAsync<TModel>(Controller controller, string name, TModel model);
+        Task<string> GetViewAsStringAsync<TModel>(Controller controller, string name, TModel model);
+        Task RenderStringAsync(Controller controller, string contents);
+        Task RenderViewAsync<TModel>(Controller controller, string name, TModel model);
     }
 
 }
--- src/imun.Http.iViewRenderer/ViewRenderer.cs
+++ src/imun.Http.iViewRenderer/ViewRenderer.cs
@@ -6,14 +6,28 @@ using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Http;
 
 namespace imun.Http.iViewRenderer {
     public class ViewRenderer: IViewRenderer {
+
         private readonly IRazorViewEngine viewEngine;
 
         public ViewRenderer(IRazorViewEngine viewEngine) => this.viewEngine = viewEngine;
 
-        public async Task<string> RenderAsync<TModel>(Controller controller, string name, TModel model) {
+        public async Task<string> GetViewAsStringAsync<TModel>(
+            Controller controller, 
+            string name, TModel model) {
+
+            if (controller == null)
+                throw new ArgumentNullException("Controller reference is null.");
+
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException("View name cannot be null or empty.");
+
+            if (model == null)
+                throw new ArgumentNullException("Model you passed is null.");
+
             string result = string.Empty;
 
             ViewEngineResult viewEngineResult = viewEngine
@@ -43,5 +57,19 @@ namespace imun.Http.iViewRenderer {
 
             return result;
         }
+
+        public async Task RenderStringAsync(Controller controller, string contents) {
+            if (string.IsNullOrWhiteSpace(contents))
+                throw new ArgumentNullException("contents cannot be null.");
+
+            await controller.Response.WriteAsync(contents);
+        }
+
+        public async Task RenderViewAsync<TModel>(Controller controller, string name, TModel model) {
+
+            var viewContents = await GetViewAsStringAsync<TModel>(controller, name, model);
+            await controller.Response.WriteAsync(viewContents);
+        }
+
     }
 }
